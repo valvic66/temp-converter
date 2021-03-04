0124e4a5e80eaf762fafc99bf0ce7066788f7669
Micu Valentin <valvic6@gmail.com>
2021-03-04 17:39:39 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Create React App
+    </div>
+  );
+}
+
+export default App;
---
+++ src/App.test.js
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/create react app/i);
+  expect(linkElement).toBeInTheDocument();
+});
